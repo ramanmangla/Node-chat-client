4ea287e6b86059762e79625e004866c8c58a47fe
Raman Mangla <ramanmangla7@gmail.com>
2019-06-24 14:46:41 -0400

    Added routing for messages using Express and functions to make AJAX requests for new messages

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+var messages = [
+	{name: 'Tim', message: 'Hi'},
+	{name: 'Jane', message: 'Hello'}
+];
+
+app.get('/messages', (req, res) => {
+	res.send(messages);
+});
+
 var server = app.listen(3000, () => {
 	console.log("Server is listening on port", server.address().port);
 });
