1f43ab61d9fd202ed53105171a07d63ff4c61e6e
Raman Mangla <ramanmangla7@gmail.com>
2019-06-26 11:39:29 -0400

    Integrated socket.io for push messages

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -21,10 +23,15 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
 	messages.push(req.body);
+	io.emit('message', req.body);
 	res.sendStatus(200);
 });
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+	console.log('Connected');
+});
+
+var server = http.listen(3000, () => {
 	console.log("Server is listening on port", server.address().port);
 });
 
