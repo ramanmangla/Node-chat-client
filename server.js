e95c3ceafa1999fe0ca39356f6031a1214eb2963
Raman Mangla <ramanmangla7@gmail.com>
2019-06-26 21:29:53 -0400

    Integrated MongoDB from mLab using Mongoose for message storage

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var credential = require('./credential.json')
 
 var app = express();
 var http = require('http').Server(app);
@@ -11,6 +13,9 @@ var io = require('socket.io')(http)
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+var dbURL = 'mongodb+srv://' + credential.username + ':' + credential.password + 
+			'@chatbotdb-pkefu.mongodb.net/test?retryWrites=true&w=majority';
 
 var messages = [
 	{name: 'Tim', message: 'Hi'},
@@ -31,7 +36,10 @@ io.on('connection', (socket) => {
 	console.log('Connected');
 });
 
+mongoose.connect(dbURL, { useNewUrlParser: true }, (err) => {
+	console.log('Mongoose connected', err);
+});
+
 var server = http.listen(3000, () => {
 	console.log("Server is listening on port", server.address().port);
 });
-
