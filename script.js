61486881b0c7c12c16b470f5b3dcc1693a7cca14
Raman Mangla <ramanmangla7@gmail.com>
2019-06-28 19:16:47 -0400

    Refactored project structure, added dependency chains and promises to improve callback nesting and added a basic word filter

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// Establishing two way connection with socket instance
+var socket = io();
+socket.on('message', addMessage);
+
+// Starts when DOM has been loaded
+$(() => {
+	// Event listener for Submit button
+    $("#send").click(()=>{
+        var message = {name: $('#name').val(), message: $('#message').val()};
+
+        postMessage(message);
+    });
+
+    getMessages();
+});
+
+// Add a message to the chat client window
+function addMessage(message){
+    $("#messages").append(`<h4> ${message.name} </h4> <p> ${message.message} </p>`);
+}
+
+// Get messages from database and populate the chat client
+function getMessages() {
+	$.get('http://localhost:3000/messages', (data) => {
+		data.forEach(addMessage);
+	});
+}
+
+// Post a message to the server
+function postMessage(message) {
+	$.post('http://localhost:3000/messages', message);
+}
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
  
 // Node Chat Client
 // Server
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -19,38 +18,61 @@ app.use(bodyParser.urlencoded({extended: false}));
 var dbURL = 'mongodb+srv://' + credential.username + ':' + credential.password + 
 			'@chatbotdb-pkefu.mongodb.net/test?retryWrites=true&w=majority';
 
+// Using ES6 promise library for Mongoose
+mongoose.Promise = Promise;
+
+// MongoDB data model
 var Message = mongoose.model('Message', {
 	name: String,
 	message: String
 });
 
+// GET /messages endpoint
 app.get('/messages', (req, res) => {
 	Message.find({}, (err, messages) => {
 		res.send(messages);
 	});
 });
 
+// POST /messages endpoint
 app.post('/messages', (req, res) => {
 	var message = new Message(req.body);
 
-	message.save((err) => {
-		if(err) {
-			sendStatus(500);
+	// Using promises and dependency chains
+	// to simplify callbacks
+	// catch block deals with any errors
+	message.save().then(() => {
+		console.log('saved');
+
+		// Looking for censored words
+		return Message.findOne({message: 'badword'});
+	}).then((censored) => {
+		if(censored) {
+			console.log('censored word found', censored);
+			return Message.remove({_id: censored.id});
 		}
 
+		// Send the the new message to all clients
+		// using the socket connection
 		io.emit('message', req.body);
 		res.sendStatus(200);
+	}).catch((err) => {
+		res.sendStatus(500);
+		console.error(error);
 	});	
 });
 
+// Establish socket connection
 io.on('connection', (socket) => {
 	console.log('Connected');
 });
 
+// Establish MongoDB connection
 mongoose.connect(dbURL, { useNewUrlParser: true }, (err) => {
 	console.log('MongooDB connected', err);
 });
 
+// Server listening on port 3000
 var server = http.listen(3000, () => {
 	console.log("Server is listening on port", server.address().port);
 });
